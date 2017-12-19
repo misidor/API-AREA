4ec61bd4e553fd2942cbf0580c079d2109799723
Matthew isidore <matt30190@gmail.com>
2017-12-19 15:12:07 +0100

    Update IModule add 2 function to set individually ITrigger & IAction

--- IModule.java
+++ IModule.java
@@ -8,11 +8,13 @@ public interface IModule
     boolean logIn(String login, String pass);
     void checkTriggers();
     List<IAction> getActions();
-    void setActions(List<IAction> m_actions);
+    void setActions(List<IAction> t_actions);
+    void addAction(IAction t_action);
     List<ITrigger> getTriggers();
-    void setTriggers(List<ITrigger> m_triggers);
+    void setTriggers(List<ITrigger> t_triggers);
+    void addTrigger(ITrigger t_trigger);
     String getDisplayName();
-    void setDisplayName(String m_displayName);
+    void setDisplayName(String t_displayName);
     void setTimeOut(long ms);
     long getTimeOut();
 }
